4eab630b14ee91efa9af7c43cd6a03e2a4a16f79
Sushruth Sastry <shastry.sushruth@gmail.com>
2021-08-10 12:34:47 -0700

    Corner case added

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,7 +1,9 @@
 import { Foo } from "lib";
 import { FooShared } from "shared";
+import { a } from "yarn-pnp-composite";
 import { Shared } from "lib2";
 
+console.log(a);
 Foo.helloWorld();
 Shared.BarShared.helloWorld();
 FooShared.helloWorld();
